e616421c757c6f4c8a69b23c0dfeb043e8b493d9
Kenert <kenert2001@gmail.com>
2021-09-23 17:24:04 +0300

    Installed React Native Elements

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
- return (
-   <View style={styles.container}>
-     <Text>Open up App.js to start working on your app!</Text>
-   </View>
- );
+  return (
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </SafeAreaProvider>
+
+  );
 };
 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   backgroundColor: '#fff',
-   alignItems: 'center',
-   justifyContent: 'center',
- },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
 export default App;
