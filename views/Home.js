343a2c415fb433999b42122a1acb6877b580d784
Tuomas <tuomakuh@metropolia.fi>
2021-09-25 14:06:50 +0300

    adjusted intro

--- views/Home.js
+++ views/Home.js
@@ -1,10 +1,8 @@
-import { View } from "react-native"
-import React from "react";
+import {View} from 'react-native';
+import React from 'react';
 
 const Home = () => {
-    return(
-        <View></View>
-    )
-}
+  return <View></View>;
+};
 
 export default Home;
